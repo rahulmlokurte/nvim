3aff4d0fbe9704d62e474cb8d9c2ecc10b470593
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-10-14 20:08:37 +0530

    Add autocommands for diagnostics

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,31 +5,27 @@
 vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
 
 -- Appearance of diagnostics
+-- Disable virtual text for diagnostics
 vim.diagnostic.config {
-  virtual_text = {
-    prefix = '●',
-    -- Add a custom format function to show error codes
-    format = function(diagnostic)
-      local code = diagnostic.code and string.format('[%s]', diagnostic.code) or ''
-      return string.format('%s %s', code, diagnostic.message)
-    end,
-  },
-  underline = false,
-  update_in_insert = true,
+  title = false,
+  underline = true,
+  virtual_text = false,
+  update_in_insert = false,
+  severity_sort = true,
   float = {
-    source = 'if_many', -- Or "if_many"
+    source = 'if_many',
+    style = 'minimal',
+    border = 'rounded',
+    header = '',
+    prefix = '',
   },
-  -- Make diagnostic background transparent
-  on_ready = function()
-    vim.cmd 'highlight DiagnosticVirtualText guibg=NONE'
-  end,
 }
 
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
   end,
   group = highlight_group,
   pattern = '*',
