07a28aa1c066085f29f8d29935818cd8da5f29ed
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-10-14 20:08:51 +0530

    Add jdtls test keymaps

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -3,6 +3,8 @@ return {
   keys = {
     { '<leader>jc', '<cmd>JdtCompile<cr>', desc = 'Java Compile' },
     { '<leader>jr', '<cmd>JdtUpdateDebugConfig<cr>', desc = 'Java Debug' },
+    { '<leader>jt', ":lua require'jdtls'.test_nearest_method()<CR>", silent = true, desc = 'Run nearest test' },
+    { '<leader>jf', ":lua require'jdtls'.test_class()<CR>", silent = true, desc = 'Run File test' },
   },
   config = function()
     local jdtls = require 'jdtls'
