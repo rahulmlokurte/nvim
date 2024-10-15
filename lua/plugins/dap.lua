8c3cd2f4d890a94f665d8c70913bd3a19de9f3ff
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-10-15 18:05:29 +0530

    Dap for javascript/typescript

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -37,6 +37,12 @@ return {
         require('nvim-dap-virtual-text').setup()
       end,
     },
+    {
+      'microsoft/vscode-js-debug',
+      version = '1.x',
+      build = 'npm i && npm run compile vsDebugServerBundle && mv dist out',
+    },
+    { 'mxsdev/nvim-dap-vscode-js' },
   },
   keys = function(_, keys)
     local dap = require 'dap'
@@ -196,5 +202,34 @@ return {
       },
     }
     require('dap-python').setup '~/.pyenv/versions/3.12.1/bin/python'
+
+    require('dap-vscode-js').setup {
+      -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+      debugger_path = vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug', -- Path to vscode-js-debug installation.
+      -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+      adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+      -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+      -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+      -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+    }
+
+    for _, language in pairs { 'typescript', 'javascript' } do
+      require('dap').configurations[language] = {
+        {
+          type = 'pwa-node',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+        },
+        {
+          type = 'pwa-node',
+          request = 'attach',
+          name = 'Attach',
+          processId = require('dap.utils').pick_process,
+          cwd = '${workspaceFolder}',
+        },
+      }
+    end
   end,
 }
