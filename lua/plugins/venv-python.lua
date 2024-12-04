4d2b2b47bc2daaaf89044e35f599ab9998ee864f
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-04 20:53:19 +0530

    Venv python notify false

--- lua/plugins/venv-python.lua
+++ lua/plugins/venv-python.lua
@@ -2,13 +2,7 @@ return {
   'linux-cultist/venv-selector.nvim',
   branch = 'regexp', -- Use this branch for the new version
   cmd = 'VenvSelect',
-  opts = {
-    settings = {
-      options = {
-        notify_user_on_venv_activation = true,
-      },
-    },
-  },
+  opts = {},
   ft = 'python',
   keys = { { '<leader>ve', '<cmd>:VenvSelect<cr>', desc = 'Select VirtualEnv', ft = 'python' } },
 }
