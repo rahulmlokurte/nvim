5e93849f655c2a8fa2fa6f5c59694d67d1fcffbd
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-06 00:18:43 +0530

    bufferline update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,13 +1,25 @@
 return {
   'akinsho/bufferline.nvim',
-  version = '*',
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  event = 'ColorScheme',
+  event = 'VeryLazy',
+  after = 'catppuccin',
   keys = {
     { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev buffer' },
     { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer' },
   },
   config = function()
-    require('bufferline').setup {}
+    local mocha = require('catppuccin.palettes').get_palette 'mocha'
+    require('bufferline').setup {
+      highlights = require('catppuccin.groups.integrations.bufferline').get {
+        styles = { 'italic', 'bold' },
+        custom = {
+          all = {
+            fill = { bg = '#000000' },
+          },
+          mocha = {
+            background = { fg = mocha.flamingo },
+          },
+        },
+      },
+    }
   end,
 }
