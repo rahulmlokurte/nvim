29118a3152a5d613810d635ac067676e0b64f189
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-11 13:47:40 +0530

    dap icons

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -124,7 +124,81 @@ return {
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {}
+    dapui.setup {
+      controls = {
+        element = 'repl',
+        enabled = true,
+        icons = {
+          disconnect = '',
+          pause = '',
+          play = '',
+          run_last = '',
+          step_back = '',
+          step_into = '',
+          step_out = '',
+          step_over = '',
+          terminate = '',
+        },
+      },
+      element_mappings = {},
+      expand_lines = true,
+      floating = {
+        border = 'single',
+        mappings = {
+          close = { 'q', '<Esc>' },
+        },
+      },
+      force_buffers = true,
+      icons = {
+        collapsed = '',
+        current_frame = '',
+        expanded = '',
+      },
+      layouts = {
+        {
+          elements = {
+            {
+              id = 'scopes',
+              size = 0.50,
+            },
+            {
+              id = 'stacks',
+              size = 0.30,
+            },
+            {
+              id = 'watches',
+              size = 0.10,
+            },
+            {
+              id = 'breakpoints',
+              size = 0.10,
+            },
+          },
+          size = 40,
+          position = 'left', -- Can be "left" or "right"
+        },
+        {
+          elements = {
+            'repl',
+            'console',
+          },
+          size = 10,
+          position = 'bottom', -- Can be "bottom" or "top"
+        },
+      },
+      mappings = {
+        edit = 'e',
+        expand = { '<CR>', '<2-LeftMouse>' },
+        open = 'o',
+        remove = 'd',
+        repl = 'r',
+        toggle = 't',
+      },
+      render = {
+        indent = 1,
+        max_value_lines = 100,
+      },
+    }
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
