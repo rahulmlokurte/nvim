31b6d448b5343559716992c18ff52feff2e185b7
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-18 17:03:46 +0530

    Adding csv support and lazy lock

---
+++ lua/plugins/csv.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'hat0uma/csvview.nvim',
+    config = function()
+      require('csvview').setup {
+        view = {
+          display_mode = 'border',
+        },
+      }
+    end,
+  },
+}
