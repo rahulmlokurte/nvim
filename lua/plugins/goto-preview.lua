c6950a00c4a7230da80b172ad338073822298a48
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-20 00:04:54 +0530

    Add goto-preview

---
+++ lua/plugins/goto-preview.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'rmagatti/goto-preview',
+    event = 'BufEnter',
+    config = function()
+      require('goto-preview').setup {
+        default_mappings = true,
+      }
+    end,
+  },
+}
