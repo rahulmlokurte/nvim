df952061f757d71bfda868251eba1159978f7536
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-27 23:24:57 +0530

    Remove keys as the awsecret plugin gives default key

--- lua/plugins/awsecret.lua
+++ lua/plugins/awsecret.lua
@@ -2,12 +2,8 @@ return {
   {
     'rahulmlokurte/awsecret.nvim',
     -- dir = '~/git/awsecret.nvim',
-    keys = {
-      { '<leader>gsm', "<cmd>lua =require('awsecret').fetch_and_cache('')<cr>", desc = 'Fetch Secrets' },
-      { '<leader>gsc', "<cmd>lua =require('awsecret').select_secret()<cr>", desc = 'Get Secrets' },
-    },
     config = function()
-      require 'awsecret'
+      require('awsecret').setup {}
     end,
   },
 }
