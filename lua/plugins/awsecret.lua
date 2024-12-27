1d0400d96706452b8761a03232403c4dc6cd0ef6
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-27 14:36:39 +0530

    Aws Secret support for input pop up

--- lua/plugins/awsecret.lua
+++ lua/plugins/awsecret.lua
@@ -1,12 +1,13 @@
 return {
   {
-    dir = '~/git/awsecret.nvim',
+    'rahulmlokurte/awsecret.nvim',
+    -- dir = '~/git/awsecret.nvim',
     keys = {
-      { '<leader>gsm', "<cmd>lua require('awsecret').fetch_and_cache('/secret/SECRET_NAME')<cr>", desc = 'Fetch Secrets' },
-      { '<leader>gsc', "<cmd>lua require('awsecret').select_secret()<cr>", desc = 'Get Secrets' },
+      { '<leader>gsm', "<cmd>lua =require('awsecret').fetch_and_cache('')<cr>", desc = 'Fetch Secrets' },
+      { '<leader>gsc', "<cmd>lua =require('awsecret').select_secret()<cr>", desc = 'Get Secrets' },
     },
     config = function()
-      require('awsecret').setup {}
+      require 'awsecret'
     end,
   },
 }
