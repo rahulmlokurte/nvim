68997cdcbef07c3d14aa9ed946c2413b1cd2fb1c
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2024-12-30 19:17:56 +0000

    fix rust codelldb path

--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local mason_registry = require 'mason-registry'
       local codelldb = mason_registry.get_package 'codelldb'
-      local extension_path = codelldb.get_install_path .. '/extension/'
+      local extension_path = codelldb:get_install_path() .. '/extension/'
       local codelldb_path = extension_path .. 'adapter/codelldb'
       local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
       local cfg = require 'rustaceanvim.config'
